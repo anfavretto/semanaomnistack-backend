fc52a65a5d9b332a99ed5959825c1fd76c499c95
Aline Favretto <aline.favretto@sap.com>
2020-03-24 21:47:54 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json()); // Express converte json em objeto
 app.use(routes);
 
