7fecc190905bbbe2a2acc2f362269d86a56df428
Aline Favretto <aline.favretto@sap.com>
2020-03-24 21:43:08 -0300

    add login controller and route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const ongController = require('./controllers/OngController');
 const incidentsController = require('./controllers/IncidentController');
 const profileController = require('./controllers/ProfileController');
+const sessionController = require('./controllers/SessionController');
 /* TIPOS DE PARÂMETROS
  * Query: parâmetros nomeados enviados na rota após o ?. Geralmente servem para filtros, paginação ?nome=valor -> pego no objeto request.query
  * Route: parâmetros utilizados para identificar recursos. /:id -> buscar pelo id -> request.params
@@ -16,5 +17,7 @@ routes.get('/incidents', incidentsController.index);
 routes.post('/incidents', incidentsController.create);
 routes.delete('/incidents/:id', incidentsController.delete);
 routes.get('/profile', profileController.index);
+
+routes.post('/sessions', sessionController.create);
 
 module.exports = routes;
