bb786cc8d106c3cd25c309b766d42e9814f87589
Aline Favretto <aline.favretto@sap.com>
2020-03-24 21:24:21 -0300

    add profile controller and route. remove unused require of incidentscontroller

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = express.Router();
 const ongController = require('./controllers/OngController');
 const incidentsController = require('./controllers/IncidentController');
+const profileController = require('./controllers/ProfileController');
 /* TIPOS DE PARÂMETROS
  * Query: parâmetros nomeados enviados na rota após o ?. Geralmente servem para filtros, paginação ?nome=valor -> pego no objeto request.query
  * Route: parâmetros utilizados para identificar recursos. /:id -> buscar pelo id -> request.params
@@ -14,5 +15,6 @@ routes.post('/ongs', ongController.create);
 routes.get('/incidents', incidentsController.index);
 routes.post('/incidents', incidentsController.create);
 routes.delete('/incidents/:id', incidentsController.delete);
+routes.get('/profile', profileController.index);
 
 module.exports = routes;
